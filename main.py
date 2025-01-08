f577df5611651bce7d247cc9a9704febf5bad519
Nelson Malbone <nhmalbone@icloud.com>
2025-01-08 18:11:56 -0500

    initial commit
    we have estiblish the connection to nasa page with the api key and using request.
    
    as of right now we are showing the date and the title of APOD

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ import config
 import streamlit as st
 import requests
 
+# title of webiste
 st.title(' Astronomy Picture of the Day')
 
+# requesting the website
 url = f'https://api.nasa.gov/planetary/apod?api_key={config.api_key}'
 response = requests.get(url)
 content = response.json()
 
-st.write(f'Date: {content["title"]}')
-st.write(f'Date: {content["date"]}')
+# streamlit website functions
+col1,col2,col3 = st.columns(3)
+st.header(f'{content["title"]}')
+
+# setting up a way to center the date
+with col1:
+    st.write("")
+with col2:
+    st.write(f'Date: {content["date"]}')
+with col3:
+    st.write("")
+
+st.image(f'{content["url"]}')
+st.write(f'Explanation: {content["explanation"]}')
